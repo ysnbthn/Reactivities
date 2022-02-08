dd3f1532cd491a479ac88424ed0c876a5fd409c9
ysnbthn <yasinbatuhanozyurek@gmail.com>
2022-02-08 11:58:39 +0300

    add mobx, make store for states, refactor code

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,108 +1,30 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect} from 'react';
 import { Container} from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './navbar';
 import ActivityDasboard from '../../features/activities/dashboard/ActivityDasboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  // use state içine activity interface arrayi ekle
-  // interface içinde activity modeli var
-  const [activities, setActivities] = useState<Activity[]>([]);
-  // activite seçimini ayarlamak için
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  // edit listesi çıkartmak için
-  const [editMode, setEditMode] = useState(false);
-  // aradaki yükleme ekranı için
-  const [loading, setLoading] = useState(true);
-  // create edit için
-  const [submitting, setSubmitting] = useState(false);
-
+  // storedan stateleri çek
+  const {activityStore} = useStore();
 
   useEffect(() => {
-    // response olarak ne geleceğini belirt
-    
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, []);
+    // storedan çek
+      activityStore.loadActivities();
+  }, [activityStore]); // dependency
 
-function handleSelectActivity(id: string) {
-  setSelectedActivity(activities.find(x=>x.id === id));
-}
-
-function handleCancelSelectActivity(){
-  setSelectedActivity(undefined);
-}
-
-function handleFormOpen(id?: string) {
-  id ? handleSelectActivity(id) : handleCancelSelectActivity();
-  setEditMode(true);
-}
-
-function handleFormClose(){
-  setEditMode(false);
-}
-
-function handleCreateOrEditActivity(activity: Activity){
-  setSubmitting(true);
-  if(activity.id) {
-    agent.Activities.update(activity).then(()=>{
-      setActivities([...activities, {...activity, id: uuid()}]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-      setSubmitting(false);
-    })
-  }else{
-    activity.id = uuid();
-    agent.Activities.create(activity).then(()=>{
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-      setSubmitting(false);
-    })
-  }
-}
-
-function handleDeleteActivity(id:string){
-  setSubmitting(true);
-  agent.Activities.delete(id).then(() => {
-    setActivities([...activities.filter(x=>x.id !== id)]);
-    setSubmitting(false);
-  })
-  setActivities([...activities.filter(x=>x.id !== id)]);
-}
-
-  if(loading){ return <LoadingComponent content='Loading app' />};
+  if(activityStore.loadingInitial){ return <LoadingComponent content='Loading app' />};
 
   return (
     <Fragment>
-      <NavBar openForm={handleFormOpen}/>
+      <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDasboard 
-        activities={activities}
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelSelectActivity}
-        editMode={editMode}
-        openForm={handleFormOpen}
-        closeForm={handleFormClose}
-        createOrEdit={handleCreateOrEditActivity}
-        deleteActivity={handleDeleteActivity}
-        submitting={submitting}
-        />
-      </Container>
-        
+        <ActivityDasboard />
+      </Container>   
     </Fragment>
   );
 }
-
-export default App;
+// observer değişimlerini takip etsin diye
+export default observer(App);
--- client-app/src/features/activities/dashboard/ActivityDasboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDasboard.tsx
@@ -1,52 +1,28 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props{
-    activities : Activity[];
-    selectedActivity : Activity | undefined;
-    selectActivity : (id: string) => void;
-    cancelSelectActivity : () => void;
-    editMode : boolean;
-    openForm : (id: string) => void;
-    closeForm : () => void;
-    createOrEdit : (activity : Activity) => void;
-    deleteActivity : (id: string) => void;
-    submitting: boolean;
-}
+// artık herşey activity storedan geliyor
+export default observer(function ActivityDasboard() {
 
-// semantic ui da grid 10 luk sistemden oluşuyor
-// Props'u destructure ediyoruz props da kullanabilirsin
-// bu şekilde birçok propu daha rahat childa gönderebiliriz
-export default function ActivityDasboard({activities, selectedActivity, selectActivity, 
-                                          cancelSelectActivity, editMode, openForm, 
-                                          closeForm, createOrEdit, deleteActivity, submitting} : Props) {
+    const {activityStore} = useStore();
+    const {selectedActivity,editMode} = activityStore;
+
     return(
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} 
-                              selectActivity={selectActivity}
-                              deleteActivity={deleteActivity}
-                              submitting={submitting}
-                              />
+                <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
-                <ActivityDetails activity={selectedActivity} 
-                cancelSelectActivity={cancelSelectActivity} 
-                openForm={openForm}
-                /> }
+                <ActivityDetails /> }
                 {editMode &&
-                <ActivityForm  
-                    closeForm={closeForm} 
-                    activity={selectedActivity} 
-                    createOrEdit={createOrEdit}
-                    submitting={submitting}
-                    /> }
+                <ActivityForm /> }
             </Grid.Column>
         </Grid>
     )
-}
+})
