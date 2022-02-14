a146eaaf01f1f8de63b0fad86acda4ca647a6dbe
ysnbthn <yasinbatuhanozyurek@gmail.com>
2022-02-14 23:07:36 +0300

    add styles to components

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -23,6 +23,16 @@ export default class ActivityStore{
     get activitiesByDate(){
         return Array.from(this.activityRegistry.values()).sort((a,b)=> Date.parse(a.date) - Date.parse(b.date));
     }
+    // activiteleri tarihe göre grupla
+    get groupedActivities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities; 
+            }, {} as {[key: string]: Activity[]})
+        )
+    }
 
     // action.bound kullanmak yerine arrow function yap
     loadActivities = async () => {
