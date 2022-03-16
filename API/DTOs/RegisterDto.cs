608bd4775fd789d0da7f388cc26013e098094027
Yasin Batuhan Özyürek <yasinbatuhanozyurek@gmail.com>
2022-03-16 14:17:22 +0300

    add missing information to password

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,7 +10,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must contain uppercase and lowercase letter and 4-8 chars long")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must contain symbol, a number, an uppercase letter, a lowercase letter and 4-8 chars long")]
         public string Password { get; set; }
         [Required]
         public string Username { get; set; }
