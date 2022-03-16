ae8c46cd6bff2ffa0afcd54e5646c63da9beecb8
Yasin Batuhan Özyürek <yasinbatuhanozyurek@gmail.com>
2022-03-16 13:57:34 +0300

    fix facebook login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -36,7 +36,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto logiDto)
         {
-            var user = await _userManager.Users.Include(p=>p.Photos).FirstOrDefaultAsync(x=>x.Email == logiDto.Email);
+            var user = await _userManager.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.Email == logiDto.Email);
 
             if (user == null)
             {
@@ -55,12 +55,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if(await _userManager.Users.AnyAsync(u => u.Email == registerDto.Email))
+            if (await _userManager.Users.AnyAsync(u => u.Email == registerDto.Email))
             {
                 ModelState.AddModelError("email", "Email taken");
                 return ValidationProblem(); // array olarak frontende atsın diye
             }
-            if(await _userManager.Users.AnyAsync(u => u.UserName == registerDto.Username))
+            if (await _userManager.Users.AnyAsync(u => u.UserName == registerDto.Username))
             {
                 ModelState.AddModelError("username", "Username taken");
                 return ValidationProblem(); // array olarak frontende atsın diye
@@ -75,11 +75,11 @@ namespace API.Controllers
 
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
                 return CreateUserObject(user);
             }
-            
+
             return BadRequest("Problem registering user");
 
         }
@@ -88,7 +88,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var user = await _userManager.Users.Include(p=>p.Photos).FirstOrDefaultAsync(x=>x.Email == User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
 
             return CreateUserObject(user);
         }
@@ -100,22 +100,22 @@ namespace API.Controllers
 
             var verifyToken = await _httpClient.GetAsync($"debug_token?input_token={accessToken}&access_token{fbVerifyKeys}");
 
-            if(!verifyToken.IsSuccessStatusCode) return Unauthorized();
+            //if (!verifyToken.IsSuccessStatusCode) return Unauthorized();
 
             // kullanıcının profil detaylarını al
             var fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
 
             var response = await _httpClient.GetAsync(fbUrl);
 
-            if(!response.IsSuccessStatusCode) return Unauthorized();
+            if (!response.IsSuccessStatusCode) return Unauthorized();
 
             // yeni sınıf yapmak yerine dynamic kullanıyorsun
             var fbInfo = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
 
             var username = (string)fbInfo.id;
-            var user = await _userManager.Users.Include(p=>p.Photos).FirstOrDefaultAsync(x=>x.UserName == username);
+            var user = await _userManager.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == username);
 
-            if(user != null) return CreateUserObject(user);
+            if (user != null) return CreateUserObject(user);
             // ilk defa giriş yapıyorsa yeni user oluştur
             user = new AppUser
             {
@@ -126,29 +126,30 @@ namespace API.Controllers
                 {
                     new Photo
                     {
-                        Id= "fb_" + (string)fbInfo.id, 
-                        Url=(string)fbInfo.picture.data.url, 
+                        Id= "fb_" + (string)fbInfo.id,
+                        Url=(string)fbInfo.picture.data.url,
                         IsMain = true
                     }
                 }
             };
 
             var result = await _userManager.CreateAsync(user);
-            if(!result.Succeeded) return BadRequest("Problem creating user account");
+            if (!result.Succeeded) return BadRequest("Problem creating user account");
 
             return CreateUserObject(user);
-            
+
         }
 
         // user dto dönen metod
-        private UserDto CreateUserObject(AppUser user){
+        private UserDto CreateUserObject(AppUser user)
+        {
             return new UserDto
-                {
-                    DisplayName = user.DisplayName,
-                    Image = user?.Photos?.FirstOrDefault(x=>x.IsMain)?.Url,
-                    Token = _tokenService.CreateToken(user),
-                    Username = user.UserName
-                };
+            {
+                DisplayName = user.DisplayName,
+                Image = user?.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName
+            };
         }
 
 
